d5c2166640b4249ebb86070a70224b5dfad8b1be
dstfvrt <dstfvrt@gmail.com>
2021-09-11 23:18:12 -0500

    revert to 46c9ffe85

--- src_fc/CqSim/Backfill.py
+++ src_fc/CqSim/Backfill.py
@@ -33,9 +33,9 @@ class Backfill:
         self.wait_job = []
     
     def backfill (self, wait_job, para_in = None):
-        # self.debug.debug("* "+self.myInfo+" -- backfill",5)
+        #self.debug.debug("* "+self.myInfo+" -- backfill",5)
         if (len(wait_job) <= 1):
-            return -1
+            return []
         self.current_para = para_in
         self.wait_job = wait_job
         job_list = self.main()
@@ -43,6 +43,7 @@ class Backfill:
     
     def main(self):
         #self.debug.debug("* "+self.myInfo+" -- main",5)
+        result = []
         if (self.mode == 1):
             # EASY backfill
             result = self.backfill_EASY()
@@ -68,10 +69,10 @@ class Backfill:
             backfill_test = self.node_module.pre_avail(self.wait_job[i]['proc'], self.current_para['time'],
                                                        self.current_para['time'] + self.wait_job[i]['run'])
             if (backfill_test == 1):
-                return self.wait_job[i]['index']
-            else:
-                i += 1
-        return -1
+                backfill_list.append(self.wait_job[i]['index'])
+                self.node_module.reserve(self.wait_job[i]['proc'], self.wait_job[i]['index'], self.wait_job[i]['run'])
+            i += 1
+        return backfill_list
         
     def backfill_cons(self):
         #self.debug.debug("* "+self.myInfo+" -- backfill_cons",5)
@@ -85,49 +86,53 @@ class Backfill:
             backfill_test = self.node_module.pre_avail(self.wait_job[i]['proc'],\
                     self.current_para['time'], self.current_para['time']+self.wait_job[i]['run'])
             if (backfill_test == 1):
-                return self.wait_job[i]['index']
-            else:
-                i += 1  
-        return -1
+                backfill_list.append(self.wait_job[i]['index'])
+            self.node_module.reserve(self.wait_job[i]['proc'], self.wait_job[i]['index'], self.wait_job[i]['run'])
+            i += 1  
+        return backfill_list
 
     def backfill_RL(self):
 
-        backfill_job = -1  # List[Job Index] - Final selected backfillable job indices
+        backfill_list = []  # List[Job Index] - Final selected backfillable job indices
 
         # Reserving First job.
         self.node_module.pre_reset(self.current_para['time'])
         self.node_module.reserve(self.wait_job[0]['proc'], self.wait_job[0]['index'], self.wait_job[0]['run'])
 
-        backfill_candidates = []
-        backfill_candidates_info = {}
-        i = 1
-        job_num = len(self.wait_job)
-        while i < job_num:
-            backfill_test = self.node_module.pre_avail(self.wait_job[i]['proc'],
-                                                       self.current_para['time'],
-                                                       self.current_para['time'] + self.wait_job[i]['run'])
-            if backfill_test == 1:
-               backfill_candidates.append(self.wait_job[i]['index'])
-               backfill_candidates_info[self.wait_job[i]['index']] = {
-                    'pos': i,
-                    'proc': self.wait_job[i]['proc'],
-                    'index': self.wait_job[i]['index'],
-                    'run': self.wait_job[i]['run'],
-                }
-            i += 1
+        while True:
+            backfill_candidates = []
+            backfill_candidates_info = {}
+            i = 1
+            job_num = len(self.wait_job)
+            while i < job_num:
+                backfill_test = self.node_module.pre_avail(self.wait_job[i]['proc'],
+                                                           self.current_para['time'],
+                                                           self.current_para['time'] + self.wait_job[i]['run'])
+                if backfill_test == 1:
+                    backfill_candidates.append(self.wait_job[i]['index'])
+                    backfill_candidates_info[self.wait_job[i]['index']] = {
+                        'pos': i,
+                        'proc': self.wait_job[i]['proc'],
+                        'index': self.wait_job[i]['index'],
+                        'run': self.wait_job[i]['run'],
+                    }
+                i += 1
 
-        if len(backfill_candidates) > 0:
-            # ************* #
-            # Reorder Queue Function [Cqsim_sim.reorder_queue()] - communicates with the Gym Environment and
-            # pushes the selected job at the beginning.
-            # ************* #
-            wait_queue = self.current_para['reorder_queue_function'](backfill_candidates)
-            selected_idx = wait_queue[0]
-            backfill_job = selected_idx
-            self.node_module.reserve(backfill_candidates_info[selected_idx]['proc'],
-                                     backfill_candidates_info[selected_idx]['index'],
-                                     backfill_candidates_info[selected_idx]['run'])
-            self.wait_job = self.wait_job[:backfill_candidates_info[selected_idx]['pos']] + \
-                            self.wait_job[backfill_candidates_info[selected_idx]['pos']+1:]
-        return backfill_job
+            if not backfill_candidates:
+                break
+            else:
+                # ************* #
+                # Reorder Queue Function [Cqsim_sim.reorder_queue()] - communicates with the Gym Environment and
+                # pushes the selected job at the beginning.
+                # ************* #
+                wait_queue = self.current_para['reorder_queue_function'](backfill_candidates)
+                selected_idx = wait_queue[0]
+                backfill_list.append(selected_idx)
+                self.node_module.reserve(backfill_candidates_info[selected_idx]['proc'],
+                                         backfill_candidates_info[selected_idx]['index'],
+                                         backfill_candidates_info[selected_idx]['run'])
+                self.wait_job = self.wait_job[:backfill_candidates_info[selected_idx]['pos']] + \
+                                self.wait_job[backfill_candidates_info[selected_idx]['pos']+1:]
+
+        return backfill_list
 
